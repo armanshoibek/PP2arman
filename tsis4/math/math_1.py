d782d05e58ea48ede197f93bddafebceea37b056
armanshoibek <157404346+armanshoibek@users.noreply.github.com>
2024-02-20 22:10:08 +0600

    Add files via upload

---
+++ tsis4/math/math_1.py
@@ -0,0 +1,20 @@
+import math
+#1)x=int(input())
+#print(math.radians(x))
+#2)base_1 = 5
+#base_2 = 6
+#height = float(input("Height of trapezoid: "))
+#base_1 = float(input('Base one value: '))
+#base_2 = float(input('Base two value: '))
+#area = ((base_1 + base_2) / 2) * height
+#print("Area is:", area)
+#3)from math import tan, pi
+#n_sides = int(input("Input number of sides: "))
+#s_length = float(input("Input the length of a side: "))
+#p_area = n_sides * (s_length ** 2) / (4 * tan(pi / n_sides))
+#print("The area of the polygon is: ",p_area)
+base=float(input())
+height=float(input())
+area=base*height
+print(area)
+
